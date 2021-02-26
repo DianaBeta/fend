14354ac0db427105e2e53460b3c088622aec996d
DianaBeta <dianabetancourt7@gmail.com>
2021-02-26 16:44:53 +0100

    sassloaders added

--- projects/evaluate-news-nlp/webpack.config.js
+++ projects/evaluate-news-nlp/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const webpack = require("webpack")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require ("html-webpack-plugin")
 module.exports = {
     entry: './src/client/index.js',
@@ -9,14 +10,22 @@ module.exports = {
                     test: '/\.js$/',
                     exclude: /node_modules/,
                     loader: "babel-loader"
-                }
+                },
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+    new CleanWebpackPlugin({
+        // Simulate the removal of files
+        dry: true,
+        // Write Logs to Console
+        verbose: true,
+        // Automatically remove all unused webpack assets on rebuild
+        cleanStaleWebpackAssets: true,
+        protectWebpackAssets: false
+})
     ]
-
 }
