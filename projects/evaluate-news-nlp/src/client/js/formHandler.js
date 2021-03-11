e55687f6cbde61b9dd3df435d43b1bedd29c529c
DianaBeta <dianabetancourt7@gmail.com>
2021-03-11 18:26:53 +0100

    feat: fetch added but broken

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -3,14 +3,66 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    // Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    console.log(process.env.API_KEY)
+    fetch('https://httpbin.org/post', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            key: "4bde5610d8cb5b0be96377106b7daf34",
+            lang: "en",
+            verbose: "y",
+            txt: "I am happy"
+
+
+        })
+    }).then(res => res.json())
+    .then(res => console.log(res));
+
+   
+    fetch('https://httpbin.org/post?key=4bde5610d8cb5b0be96377106b7daf34&lang=en&txt=I%20am%20happy', {
+    method: 'post',
+    headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+    },
+
+    }).then(res => res.json())
+    .then(res => console.log(res));
+    
+    let baseURL ='https://api.meaningcloud.com/sentiment-2.1?key=';
+    
+    
+    // working fetch, above debugging and visualisation of post requests
+    fetch('https://api.meaningcloud.com/sentiment-2.1?key=4bde5610d8cb5b0be96377106b7daf34&lang=en&txt=I%20am%20happy', {
+        method: "POST",
+        /*body: JSON.stringify({
+            key: Client.textapi,
+            lang: "en",
+            verbose: "y",
+            txt: "I am happy"
+
+
+        })*/
     })
-}
+    .then((res) => {
+        console.log("Request complete! response:", res.json());
+      })
+
+        /*body: JSON.stringify({
+            key: Client.textapi,
+            lang: "en",
+            verbose: "y",
+            txt: "I am happy"
+
+
+        })*/
+    }
+
 
 export { handleSubmit }
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,10 +1,11 @@
+/*Emty js object to act as an endpoint for all routes*/
+projectData = {};
+
 const dotenv = require('dotenv');
 dotenv.config();
 console.log(`Your API key is ${process.env.API_KEY}`);
 
-var textapi = new aylien({
-    application_key: process.env.API_KEY
- });
+var textapi = process.env.API_KEY;
  
 var path = require('path')
 const express = require('express')
@@ -16,16 +17,44 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
-app.get('/', function (req, res) {
+app.get('/add', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
-
+//POST route: sends the data to the project endpoint
+app.post('.all', function (req, res) {
+    let data = req.body;
+});
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+//app.get('/test', function (req, res) {
+//    res.send(mockAPIResponse)
+//})
+
+   // working fetch, above debugging and visualisation of post requests
+   fetch('https://api.meaningcloud.com/sentiment-2.1?key=4bde5610d8cb5b0be96377106b7daf34&lang=en&txt=I%20am%20happy', {
+    method: "POST",
+    /*body: JSON.stringify({
+        key: Client.textapi,
+        lang: "en",
+        verbose: "y",
+        txt: "I am happy"
+
+
+    })*/
 })
+.then((res) => {
+    console.log("Request complete! response:", res.json());
+  })
+
+    /*body: JSON.stringify({
+        key: Client.textapi,
+        lang: "en",
+        verbose: "y",
+        txt: "I am happy"
+
+
+    })*/
